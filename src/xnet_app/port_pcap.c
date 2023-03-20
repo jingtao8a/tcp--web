118988791a24fffdf603af4bc55405b52e47576c
yuxintao <1921056015@qq.com>
2023-03-20 14:55:24 +0800

    initial commit

---
+++ src/xnet_app/port_pcap.c
@@ -0,0 +1,96 @@
+﻿/**
+ * 用1500行代码从0开始实现TCP/IP协议栈+WEB服务器
+ *
+ * 本源码旨在用最简单、最易懂的方式帮助你快速地了解TCP/IP以及HTTP工作原理的主要核心知识点。
+ * 所有代码经过精心简化设计，避免使用任何复杂的数据结构和算法，避免实现其它无关紧要的细节。
+ *
+ * 作者：李述铜
+ * 微信公众号：01课堂
+ * 网址：https://www.yuque.com/lishutong-docs
+ *
+ * 版权声明：源码仅供学习参考，请勿用于商业产品，不保证可靠性。二次开发或其它商用前请联系作者。
+ *
+ * 注意：本课程提供的tcp/ip实现很简单，只能够用于演示基本的协议运行机制。我还开发了另一套更加完整的课程，
+ * 展示了一个更加完成的TCP/IP协议栈的实现。功能包括：
+ * 1. IP层的分片与重组
+ * 2. Ping功能的实现
+ * 3. TCP的流量控制等
+ * 4. 基于UDP的TFTP服务器实现
+ * 5. DNS域名接触
+ * 6. HTTP服务器
+ * 7. 提供socket接口供应用程序使用
+ * 8、代码可移植，可移植到arm和x86平台上
+ * ..... 更多功能开发中...........
+ * 如果你有兴趣的话，请扫仓库中的二维码，或者点击以上面的链接可找到该课程。
+ */
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "pcap_device.h"
+#include "xnet_tiny.h"
+
+static pcap_t * pcap;
+
+// pcap所用的网卡
+const char * ip_str = "192.168.159.1";      // 根据实际电脑上存在的网卡地址进行修改
+const char my_mac_addr[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
+/**
+ * 初始化网络驱动
+ * @return 0成功，其它失败
+ */
+xnet_err_t xnet_driver_open (uint8_t * mac_addr) {
+    memcpy(mac_addr, my_mac_addr, sizeof(my_mac_addr));
+    pcap = pcap_device_open(ip_str, mac_addr, 1);
+    if (pcap == (pcap_t *)0) {
+        exit(-1);
+    }
+    return XNET_ERR_OK;
+}
+
+/**
+ * 发送数据
+ * @param frame 数据起始地址
+ * @param size 数据长度
+ * @return 0 - 成功，其它失败
+ */
+xnet_err_t xnet_driver_send (xnet_packet_t * packet) {
+    return pcap_device_send(pcap, packet->data, packet->size);
+}
+
+/**
+ * 读取数据
+ * @param frame 数据存储位置
+ * @param size 数据长度
+ * @return 0 - 成功，其它失败
+ */
+xnet_err_t xnet_driver_read (xnet_packet_t ** packet) {
+    uint16_t size;
+    xnet_packet_t * r_packet = xnet_alloc_for_read(XNET_CFG_PACKET_MAX_SIZE);
+
+    size = pcap_device_read(pcap, r_packet->data, XNET_CFG_PACKET_MAX_SIZE);
+    if (size) {
+        r_packet->size = size;
+        *packet = r_packet;
+        return XNET_ERR_OK;
+    }
+
+    return XNET_ERR_IO;
+}
+
+/**
+ * 获取自程序启动以来，过去了多长时间
+ * @return 程序的系统时间
+ */
+const xnet_time_t xsys_get_time(void) {
+    static uint32_t pre = 0;
+
+    // 以下部分仅供调试时使用
+#if 0
+    uint32_t c = clock() / CLOCKS_PER_SEC;
+    if (c != pre) {
+        printf("sec: %d, 100ms: %d\n", c, (xnet_time_t)(clock()  * 10 / CLOCKS_PER_SEC));
+        pre = c;
+    }
+#endif
+    return (xnet_time_t)(clock() / CLOCKS_PER_SEC);
+}
---
+++ src/xnet_app/xserver_datetime.c
@@ -0,0 +1,68 @@
+﻿/**
+ * 用1500行代码从0开始实现TCP/IP协议栈+WEB服务器
+ *
+ * 本源码旨在用最简单、最易懂的方式帮助你快速地了解TCP/IP以及HTTP工作原理的主要核心知识点。
+ * 所有代码经过精心简化设计，避免使用任何复杂的数据结构和算法，避免实现其它无关紧要的细节。
+ *
+ * 作者：李述铜
+ * 微信公众号：01课堂
+ * 网址：https://www.yuque.com/lishutong-docs
+ *
+ * 版权声明：源码仅供学习参考，请勿用于商业产品，不保证可靠性。二次开发或其它商用前请联系作者。
+ *
+ * 注意：本课程提供的tcp/ip实现很简单，只能够用于演示基本的协议运行机制。我还开发了另一套更加完整的课程，
+ * 展示了一个更加完成的TCP/IP协议栈的实现。功能包括：
+ * 1. IP层的分片与重组
+ * 2. Ping功能的实现
+ * 3. TCP的流量控制等
+ * 4. 基于UDP的TFTP服务器实现
+ * 5. DNS域名接触
+ * 6. HTTP服务器
+ * 7. 提供socket接口供应用程序使用
+ * 8、代码可移植，可移植到arm和x86平台上
+ * ..... 更多功能开发中...........
+ * 如果你有兴趣的话，请扫仓库中的二维码，或者点击以上面的链接可找到该课程。
+ */
+#include "xserver_datetime.h"
+#include <string.h>
+#include <time.h>
+
+#define TIME_STR_SIZE       128         // 时间字符串存储长度
+
+static xnet_err_t datetime_handler (xudp_t * udp, xipaddr_t * src_ip, uint16_t src_port, xnet_packet_t * packet) {
+    xnet_packet_t * tx_packet;
+    time_t rawtime;
+    const struct tm * timeinfo;
+
+    tx_packet = xnet_alloc_for_send(TIME_STR_SIZE);
+    if (tx_packet == (xnet_packet_t *)0) {
+        return XNET_ERR_MEM;
+    }
+
+    // 参见：http://www.cplusplus.com/reference/ctime/localtime/
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+
+    // strftime参见：http://www.cplusplus.com/reference/ctime/strftime/
+    // Weekday, Month Day, Year Time-Zone
+    strftime((char *)tx_packet->data, TIME_STR_SIZE, "%A, %B %d, %Y %T-%z", timeinfo);
+    truncate_packet(tx_packet, (uint16_t)strlen((char *)tx_packet->data));
+
+    return xudp_out(udp, src_ip, src_port, tx_packet);
+}
+
+xnet_err_t xserver_datetime_create(uint16_t port) {
+    xnet_err_t err;
+
+    xudp_t* udp = xudp_open(datetime_handler);
+    if (udp == (xudp_t*)0) {
+        return -1;
+    }
+
+    err = xudp_bind(udp, port);
+    if (err < 0) {
+        xudp_close(udp);
+        return err;
+    }
+    return 0;
+}
